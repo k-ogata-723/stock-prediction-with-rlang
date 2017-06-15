8c137e857884a764c62d6e4b3c95931d8a77ea08
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-06-15 23:12:55 +0900

    Multiple linear regression user lm

--- stock-prediction-with-rlang/stock-prediction.R
+++ stock-prediction-with-rlang/stock-prediction.R
@@ -1,24 +1,14 @@
-
 stock <- read.csv("stock-sony-250.csv")
-
 
 # 1行目を削除
 stockData <- stock[2:240, 2:7]
-print(stockData)
+# print(stockData)
 
 # 終値をyとしてデータにする
 y <- stock[ ,5]
+# print(y)
 
-# 終値以外のデータを取り出し
-dataRegressionTrain <- stockData[ , -5]
 dataRegressionTrain <- stockData[ , -1]
-
-print(length(dataRegressionTrain[ , 1]))
-print(length(dataRegressionTrain[ , 2]))
-print(length(dataRegressionTrain[ , 3]))
-print(length(dataRegressionTrain[ , 4]))
-print(length(dataRegressionTrain[ , 5]))
-# print(dataRegressionTrain)
 
 # それぞれのデータを取り出し
 lowPrice <- stock[ , 4]
@@ -26,11 +16,26 @@ highPrice <- stock[ , 3]
 date <- stock[ , 1]
 
 # 確認用にlowPriceをプロット
-plot(date, lowPrice, col="blue", main="plot row stock data", xlab="date", ylab="price")
+# plot(date, lowPrice, col="blue", main="plot row stock data", xlab="date", ylab="price")
+
+# data.frame型にまとめていく
+# 始値と出来高と売買代金を利用する
+dataRegressionTrain <- data.frame(
+  y = stock[ ,5],
+  x1 = stock[ ,2],
+#   x2 = stock[ ,3],
+#   x3 = stock[ ,4],
+  x4 = stock[ ,6],
+  x5 = stock[ ,7]
+)
+
 
 modelLm <- lm(
-  y ~ .,
+  y ~(.)^2,
   data = dataRegressionTrain
 )
+
+sum <- summary(modelLm)
+sum
 
 modelLm <- step(modelLm)
