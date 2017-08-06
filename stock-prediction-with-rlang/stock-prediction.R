c707a322556be9c43af7f29f2080fdf127884b77
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-08-06 18:01:12 +0900

    Plot lm(top.1000.sites)

--- stock-prediction-with-rlang/stock-prediction.R
+++ stock-prediction-with-rlang/stock-prediction.R
@@ -1,14 +1,23 @@
 stock <- read.csv("stock-sony-250.csv")
+stock_2015 <- read.csv("stocks-sony-2015.csv")
+stock_2016 <- read.csv("stocks-sony-2016.csv")
+stock_2017 <- read.csv("stocks-sony-2017.csv")
+
 
 # 1行目を削除
 stockData <- stock[2:240, 2:7]
 # print(stockData)
 
 # 終値をyとしてデータにする
-y <- stock[ ,5]
+# y <- stock[ ,5]
 # print(y)
 
-dataRegressionTrain <- stockData[ , -1]
+# trainデータ
+stock_train <- rbind(stock_2017, stock_2016)
+print(stock_train)
+
+# trainデータ
+stock_test <- stock_2017
 
 # それぞれのデータを取り出し
 # 学習には利用していない
@@ -23,20 +32,20 @@ date <- stock[ , 1]
 # data.frame型にまとめていく
 # 始値と出来高と売買代金を利用する
 
-## train データ
+## train データをdata.frame型に変換
 dataRegressionTrain <- data.frame(
-  y = stock[ ,5],
-  x1 = stock[ ,2],
-  x4 = stock[ ,6],
-  x5 = stock[ ,7]
+  y = stock_train[ ,5],
+  x1 = stock_train[ ,2],
+  x4 = stock_train[ ,6],
+  x5 = stock_train[ ,7]
 )
 
 ## test データ
-dataRegressionTrain <- data.frame(
-y = stock[ ,5],
-x1 = stock[ ,2],
-x4 = stock[ ,6],
-x5 = stock[ ,7]
+dataRegressionTest <- data.frame(
+y = stock_test[ ,5],
+x1 = stock_test[ ,2],
+x4 = stock_test[ ,6],
+x5 = stock_test[ ,7]
 )
 
 
@@ -53,5 +62,7 @@ print(sum)
 modelLm <- step(modelLm)
 
 # データと重回帰結果をプロットしていく
-plot(lowPrice, highPrice)
+par(mfrow=c(2,2))
+plot(modelLm)
 lines(lowPrice, fitted(modelLm))
+predict(modelLm)
